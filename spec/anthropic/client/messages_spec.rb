61cbbe06134f2fe82c39090d309f5b93ec2f622d
swombat <daniel.github@tenner.org>
2024-05-07 19:35:50 +0100

    Tidy up

--- spec/anthropic/client/messages_spec.rb
+++ spec/anthropic/client/messages_spec.rb
@@ -32,7 +32,6 @@ RSpec.describe Anthropic::Client do
 
       let(:stream) do
         proc do |chunk|
-          puts chunk.inspect
           chunks << chunk
         end
       end
@@ -68,7 +67,6 @@ RSpec.describe Anthropic::Client do
 
       let(:stream) do
         proc do |chunk|
-          puts chunk.inspect
           chunks << chunk
         end
       end
@@ -93,37 +91,5 @@ RSpec.describe Anthropic::Client do
         end
       end
     end
-
-    #   context "with a prompt and max_tokens", :vcr do
-    #     let(:prompt) { "How high is the sky?" }
-    #     let(:max_tokens) { 5 }
-
-    #     let(:response) do
-    #       Anthropic::Client.new(access_token: ENV.fetch("ANTHROPIC_API_KEY", nil)).messages(
-    #         parameters: {
-    #           model: model,
-    #           messages: [
-    #             {
-    #               role: "user",
-    #               content: "How High is the Sky?"
-    #             }
-    #           ],
-    #           max_tokens: max_tokens
-    #         }
-    #       )
-    #     end
-    #     let(:text) { response.dig("choices", 0, "text") }
-    #     let(:cassette) { "#{model} complete #{prompt}".downcase }
-
-    #     context "with model: claude-2.1" do
-    #       let(:model) { "claude-2.1" }
-
-    #       it "succeeds" do
-    #         VCR.use_cassette(cassette) do
-    #           expect(response["content"].empty?).to eq(false)
-    #         end
-    #       end
-    #     end
-    #   end
   end
 end
