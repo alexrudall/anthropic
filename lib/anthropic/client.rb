ca3a8b2fab8624ae59976d8379e57a82a290a631
ignacio-chiazzo <ignaciochiazzo@gmail.com>
2024-10-09 22:08:26 -0300

    Introduce Batch class to delegate methods

--- lib/anthropic/client.rb
+++ lib/anthropic/client.rb
@@ -1,3 +1,5 @@
+require_relative "batch"
+
 module Anthropic
   class Client
     include Anthropic::HTTP
@@ -63,53 +65,10 @@ module Anthropic
     #   "stop_sequence" => nil,
     #   "usage" => {"input_tokens" => 15, "output_tokens" => 5}
     # }
-    def messages(parameters: {})
-      json_post(path: "/messages", parameters: parameters)
-    end
+    def messages(**args)
+      return Batch.new(self) unless args && args[:parameters]
 
-    # Anthropic API Parameters as of 2024-10-09:
-    #   @see https://docs.anthropic.com/en/api/creating-message-batches
-    #
-    # @param [Array] :requests - List of requests for prompt completion. Each is an individual request to create a Message.
-    #   Requests are an array of hashes, each with the following keys:
-    #   - :custom_id (required): Developer-provided ID created for each request in a Message Batch.
-    #                            Useful for matching results to requests, as results may be given out of request order.
-    #                            Must be unique for each request within the Message Batch.
-    #   - :params (required): Messages API creation parameters for the individual request.
-    #                            See the Messages API reference for full documentation on available parameters.
-    #
-    # @returns [Hash] the response from the API (after the streaming is done, if streaming)
-    #   @example:
-    # {
-    #   "id"=>"msgbatch_01668jySCZeCpMLsxFcroNnN",
-    #   "type"=>"message_batch",
-    #   "processing_status"=>"in_progress",
-    #   "request_counts"=>{"processing"=>2, "succeeded"=>0, "errored"=>0, "canceled"=>0, "expired"=>0},
-    #   "ended_at"=>nil,
-    #   "created_at"=>"2024-10-09T20:18:11.480471+00:00",
-    #   "expires_at"=>"2024-10-10T20:18:11.480471+00:00",
-    #   "cancel_initiated_at"=>nil,
-    #   "results_url"=>nil
-    # }
-    def batch_messages(requests_parameters)
-      # required to use the Batch API. https://docs.anthropic.com/en/docs/build-with-claude/message-batches
-      custom_headers = { "anthropic-beta" => "message-batches-2024-09-24" }
-      json_post(path: "/messages/batches", parameters: { "requests" => requests_parameters }, custom_headers:)
-    end
-
-    # allows using the batch API via `client.messages.batch` method.
-    def messages
-      MessagesBatcher.new(self)
-    end
-
-    class MessagesBatcher
-      def initialize(client)
-        @client = client
-      end
-
-      def batch(requests_parameters)
-        @client.batch_messages(requests_parameters)
-      end
+      json_post(path: "/messages", parameters: args[:parameters])
     end
 
     private
--- spec/anthropic/client/messages/batch_spec.rb
+++ spec/anthropic/client/messages/batch_spec.rb
@@ -4,41 +4,41 @@ RSpec.describe Anthropic::Client do
 
   describe "#messages.batch" do
     context "batch messages" do
-      let(:request_1) do
+      let(:request1) do
         {
           custom_id: "first-prompt-in-my-batch",
           params: {
-              model: "claude-3-haiku-20240307",
-              max_tokens: 100,
-              messages: [
-                  {
-                      "role": "user",
-                      "content": "Hey Claude, tell me a short fun fact about video games!",
-                  }
-              ],
-          },
+            model: "claude-3-haiku-20240307",
+            max_tokens: 100,
+            messages: [
+              {
+                role: "user",
+                content: "Hey Claude, tell me a short fun fact about video games!"
+              }
+            ]
+          }
         }
       end
 
-      let(:request_2) do
+      let(:request2) do
         {
           custom_id: "second-prompt-in-my-batch",
           params: {
-              model: "claude-3-5-sonnet-20240620",
-              max_tokens: 100,
-              "messages": [
-                  {
-                      "role": "user",
-                      "content": "Hey Claude, tell me a short fun fact about bees!",
-                  }
-              ],
-          },
+            model: "claude-3-5-sonnet-20240620",
+            max_tokens: 100,
+            messages: [
+              {
+                role: "user",
+                content: "Hey Claude, tell me a short fun fact about bees!"
+              }
+            ]
+          }
         }
       end
 
       let(:response) do
-        Anthropic::Client.new(access_token: ENV.fetch("ANTHROPIC_API_KEY", nil)).batch_messages(
-          [request_1, request_2]
+        Anthropic::Client.new(access_token: ENV.fetch("ANTHROPIC_API_KEY", nil)).messages.batch(
+          [request1, request2]
         )
       end
 
